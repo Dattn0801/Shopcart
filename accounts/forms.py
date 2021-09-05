aad120e1d02a676831fc67f2fd7680c6af89843f
Dattn0801 <thanhdat08011999@gmail.com>
2021-09-06 05:56:40 +0700

    today so bad

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,7 @@ from .models import Account
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput(attrs={
         'placeholder': 'Enter Password',
+        'class': 'form-control',
     }))
     confirm_password = forms.CharField(widget = forms.PasswordInput(attrs={
         'placeholder': 'Confirm Password',
@@ -25,3 +26,13 @@ class RegistrationForm(forms.ModelForm):
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Password does not match!'
+            )
--- shopcart/settings.py
+++ shopcart/settings.py
@@ -137,3 +137,16 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'thanhdatcute888@gmail.com'
+EMAIL_HOST_PASSWORD = 'thanhdatcute777'
+EMAIL_USE_TLS = True
