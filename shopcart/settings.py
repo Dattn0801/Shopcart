ef89e794faebadd650d0ae016885b0e3fa097c5e
Dattn0801 <thanhdat08011999@gmail.com>
2021-09-07 02:46:16 +0700

    login,logout,forgotpassword,resetpassword done

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -147,6 +147,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thanhdatcute888@gmail.com'
-EMAIL_HOST_PASSWORD = 'thanhdatcute777'
+EMAIL_HOST_USER = 'thanhdat08011999@gmail.com'
+EMAIL_HOST_PASSWORD = 'thanhdatcute1999'
 EMAIL_USE_TLS = True
